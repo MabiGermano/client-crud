a2536d087a58d1c240887fe06802a2809b811b3b
MabiGermano <mabi.germano.e@gmail.com>
2021-07-27 13:03:43 -0300

    checking and writing documentation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# client-crud
+# Client crud
+
+> Client crud project builded using Java 11 and Spring boot. 
+> Other technologies: gradle, swagger, H2 database and tested with JUnit.
+
+🖥️ When start projet you can access at ```8080``` port.
+
+📃 The API documentation can be accessed at ```http://localhost:8080/swagger-ui.html/```
--- src/main/java/br/com/challenge/clientcrud/SwaggerConfig.java
+++ src/main/java/br/com/challenge/clientcrud/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package br.com.challenge.clientcrud.config;
+package br.com.challenge.clientcrud;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfig {
 
 	@Bean
     public Docket docket() {
